f6f1159a6f413aea3f4670fc1ce511c5c365b60f
Omar Meharab <omarmeharab81@gmail.com>
2021-02-06 14:18:42 +0600

    ES6 practices

---
+++ letconst.js
@@ -0,0 +1,9 @@
+const wifey = 'giyo sana';
+wifey = 'mina san';
+console.log(wifey);
+
+let sum = 0;
+for (let i = 0; i < array.length; i++) {
+    sum = sum + i;
+}
+console.log(i);
---
+++ string.js
@@ -0,0 +1,13 @@
+const firstName = 'justin';
+const lastName = 'timberlake';
+const fullName = firstName + ' ' + lastName;
+const fullName2 = `${firstName} ${lastName} is a good lad`;
+
+const multiline = "what\n"
+    + "is\n"
+    + "wrong"
+
+const multiline2 = `hey
+man
+sup`
+console.log(multiline2);
